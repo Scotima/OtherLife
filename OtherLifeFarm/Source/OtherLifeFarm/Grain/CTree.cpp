7afcaaea3b3b9085ee22c58370e61ba2b9be21d1
Kihold <gminkyu01@naver.com>
2025-02-18 21:55:33 +0900

    ㅁㅇ
    ㅁㅇ

--- OtherLifeFarm/Source/OtherLifeFarm/Grain/CTree.cpp
+++ OtherLifeFarm/Source/OtherLifeFarm/Grain/CTree.cpp
@@ -4,6 +4,16 @@ ACTree::ACTree()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> TreeAsset(TEXT("/Game/Simple_forest/Meshes/Trees/SM_tree_01"));
+
+	if (TreeAsset.Succeeded())
+	{
+		MeshComp->SetStaticMesh(TreeAsset.Object);
+	}
+
 }
 
 void ACTree::BeginPlay()
